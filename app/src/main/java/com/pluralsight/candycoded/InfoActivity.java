254e331179a31cd30d91bf762d84f36d5dd404e7
LeratoSbk <52829993+LeratoSbk@users.noreply.github.com>
2020-10-04 17:23:47 +0200

    Check My Work

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import java.net.URI;
+
 public class InfoActivity extends AppCompatActivity {
 
     @Override
@@ -43,4 +45,8 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        URI uriPhone= URI.create(String.valueOf(Uri.parse("tel:0123456789")));
+    }
 }
